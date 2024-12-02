6b3c26aeaaa914f0cac83fe5fb9e1bc5f25af05a
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2024-12-02 12:43:35 +0700

    Upload template file

---
+++ template.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+ROOT = Path(__file__).parent.parent.parent
+DATA_FILENAME = Path(__file__).name.replace('.py', '.txt')
+TYPE = Path(__file__).parent.name # "gold" or "silver"
+INPUT = ROOT / 'data' / 'input' / DATA_FILENAME
+OUTPUT = ROOT / 'data' / 'output' / TYPE / DATA_FILENAME
+
+REOUTPUT_INPUT = False
+
+def solve(lines: list[str]) -> int:
+    total = 0
+    values = []
+        
+    # ===== Code Here =====
+    pass
+    # =====================
+        
+    total = sum(values)
+    return total, values
+
+def main():
+    lines = []
+    
+    # Read the input file
+    with open(INPUT, 'r') as f:
+        lines = f.readlines()
+    
+    sum, values = solve(lines)
+    print(f"Sum: {sum}\n")
+            
+    # Write the output
+    with open(OUTPUT, 'w') as f:
+        f.write(str(sum) + '\n\n')
+        f.write(str(values) + '\n\n')
+        
+        if REOUTPUT_INPUT:
+            for i, value in enumerate(values):
+                f.write(f'{lines[i].strip()} -> {value}\n')
+        
+if __name__ == '__main__':
+    main()
